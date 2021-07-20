f135a960e6887be6b1b49fce8e1240d9e7caa302
Cade Ellsworth <caedmd2100@gmail.com>
2021-07-20 14:32:38 -0700

    should be working

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,58 +1,93 @@
-const APP_PREFIX = 'BudgetTracker-';
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
+const CACHE_NAME = 'my-site-cache-budget';
+const DATA_CACHE_NAME = 'data-cache-budget';
 
 const FILES_TO_CACHE = [
-    "./models.transaction.js",
-    "./public/css/style.css",
-    "./public/js/index.js",
-    "./public/index.html",
-    "./routes/api.js"
+    "/",
+    "/css/styles.css",
+    "/manifest.json",
+    "/index.html",
+    "/js/index.js",
+    "/js/idb.js",
+    "/icons/icon-72x72.png",
+    "/icons/icon-96x96.png",
+    "/icons/icon-128x128.png",
+    "/icons/icon-144x144.png",
+    "/icons/icon-152x52.png",
+    "/icons/icon-192x192.png",
+    "/icons/icon-384x384.png",
+    "/icons/icon-512x512.png",
 ];
 
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache: ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
+self.addEventListener('install', function (evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Your files were pre-cached successfully!');
+            return cache.addAll(FILES_TO_CACHE);
         })
-    )
+    );
+
+    self.skipWaiting();
 });
+
+// Activate the service worker and remove old data from the cache
+// YOUR CODE HERE
+//
 
 self.addEventListener('activate', function (e) {
     e.waitUntil(
-        caches.keys().then(function (keyList) {
-            let cacheKeepList = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX);
-            });
-            cacheKeepList.push(CACHE_NAME);
-
+        caches.keys().then(keyList => {
             return Promise.all(
-                keyList.map(function (key, i) {
-                    if (cacheKeepList.indexOf(key) === -1) {
-                        console.log('deleting cache: ' + keyList[i]);
-                        return caches.delete(keyList[i]);
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('Removing all old cache data', key);
+                        return caches.delete(key);
                     }
                 })
             );
         })
     );
+
+    self.clients.claim();
 });
 
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) { // if cache is available, respond with cache
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } else {       // if there are no cache, try fetching request
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
-            }
+// Intercept fetch requests
+// YOUR CODE HERE
+//
 
-            // You can omit if/else for console.log & put one line below like this too.
-            // return request || fetch(e.request)
-        })
-    )
-})
+self.addEventListener('fetch', function (evt) {
+    if (evt.request.url.includes('/api/')) {
+        evt.respondWith(
+            caches
+                .open(DATA_CACHE_NAME)
+                .then(cache => {
+                    return fetch(evt.request)
+                        .then(response => {
+                            if (response.status === 200) {
+                                cache.put(evt.request.url, response.clone());
+                            }
+
+                            return response;
+                        })
+                        .catch(err => {
+                            return cache.match(evt.request);
+                        });
+                })
+                .catch(err => console.log(err))
+        );
+        return;
+    }
+    else {
+        evt.respondWith(
+            fetch(evt.request).catch(function () {
+                return caches.match(evt.request).then(function (response) {
+                    if (response) {
+                        return response;
+                    } else if (evt.request.headers.get('accept').includes('text/html')) {
+                        // return the cached home page for all requests for html pages
+                        return caches.match('/');
+                    }
+                });
+            })
+        );
+    }
+});
